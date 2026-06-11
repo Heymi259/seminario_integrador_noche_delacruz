a7767bd08313c65de464cee261e9e5b56e782138
unknown <heymiced34@gmail.com>
2026-06-11 18:11:03 -0500

    COMMIT DEL DÍA LUNES 08, MARTES 09 Y MIERCOLES 10

--- modulo2/shopapi/store/serializers/category.py
+++ modulo2/shopapi/store/serializers/category.py
@@ -1,4 +1,4 @@
-# store/serializers/category.py — reemplazar CategorySerializer
+# store/serializers/category.py
 from rest_framework import serializers
 from django.utils.text import slugify
 from store.models import Category
--- modulo2/shopapi/store/views/health.py
+++ modulo2/shopapi/store/views/health.py
@@ -1,5 +1,4 @@
 # store/views/health.py
-
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
@@ -8,18 +7,21 @@ from rest_framework.response import Response
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def health_check(request):
-    return Response({
-        'service': 'ShopApi',
-        'status': 'ok',
-        'version': '1.0',
-        'message': 'Welcome to ShopApi!'
-    })
+    return Response(
+        {'Service': 'ShopApi', 
+         'status': 'ok',
+         'version': '1.0'})
+
 
 
 @api_view(['GET'])
 @permission_classes([AllowAny])
-def testingcicd(request):
-    return Response({
-        'status': 'success',
-        'message': 'CI/CD working correctly!'
-    })
+def system_info(request):
+    return Response(
+        {
+            'service': 'ShopApi',
+            'status': 'running',
+            'environment': 'production',
+            'version': '1.1'
+        }
+    )
